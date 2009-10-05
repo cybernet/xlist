a3c9fb2038413f57d57edf73b2da7232008dacd1
cybernet <cybernet2u@2reply.in>
2009-10-05 20:35:34 +0000

    dilbermc theme added / http://tracker.cyberfun.ro/ \/\/ cybernet / 10/05/09

---
+++ tracker/smf/Themes/dilbermc/languages/Settings.german.php
@@ -0,0 +1,34 @@
+<?php
+// Version: 1.1; Settings
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Dilber Multicolor Theme von HarzeM.<br /><br />Professionelles, schlankes Theme in mehreren Farbvarianten.';
+
+$txt['logo_not_show'] = 'Logo im Header ausblenden:';
+
+$txt['page_div_width'] = 'Breite:';
+$txt['page_div_width_desc'] = 'Kann in <b>Pixeln </b>, z.B. <b>800px</b>, oder in <b>Prozent</b>, z.B. <b>75%</b>, angegeben werden. Die Angabe muss allerdings mit <b>px oder %</b> enden!';
+$txt['show_name_near_logo'] = 'Forumname neben Logo anzeigen:';
+
+$txt['top_right_html'] = 'HTML-Code, der oben rechts angezeigt werden soll:';
+$txt['top_right_html_desc'] = 'Hier können Links zu anderen Webseiten, Banner, etc. dargestellt werden.';
+
+$txt['custom_global_html'] = 'HTML-Code, welcher unter dem "User Info"-Block angezeigt werden soll:';
+$txt['custom_global_html_desc'] = 'HTML-Code wird zwischen Seiteninhalt und "User Info"-Block dargestellt. Hier kann ein Banner oder andere Informationen dargestellt werden.';
+
+$txt['child_boards_rows'] = 'Untergeordnete Boards in einer Spalte darstellen, statt in zwei Spalten:';
+$txt['child_boards_rows_desc'] = 'Wenn aktiviert, werden untergeordnete Boards in einer einzigen Spalte dargestellt, statt in zwei Spalten.';
+
+$txt['boardname_posts_lastposts_nodisplay'] = 'Anzeige von "Boardname", "Beiträge", "Letzte Beiträge" auf dem Board Index ausblenden:';
+$txt['boardname_posts_lastposts_nodisplay_desc'] = 'Wenn aktiv, wird die Titelzeile "Boardname", "Beiträge", "Letzte Beiträge" im Board Index ausgeblendet.';
+
+$txt['custom_copyright'] = 'Copyright, welches unter dem SMF-Copyright angezeigt werden soll:';
+$txt['custom_copyright_desc'] = 'Du kannst HTML-Code verwenden.<br/><br/><hr/><hr/><br/>';
+
+$txt['allow_color_change'] = 'Farbauswahl im Index erlauben?';
+$txt['allow_color_change_no'] = 'Nicht erlauben';
+$txt['allow_color_change_box'] = 'Als Auswahlliste anzeigen';
+$txt['allow_color_change_bars'] = 'Als anklickbare Farbflächen anzeigen';
+
+$txt['theme_main_color'] = 'Theme-Farbe:';
+?>
