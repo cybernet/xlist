44c369e3b1294084c9516c5028cf77c3f1a86414
cybernet <cybernet2u@2reply.in>
2009-10-05 12:13:48 +0000

    2 new themes / http://tracker.cyberfun.ro/ \/\/ cybernet / 10/05/09

---
+++ tracker/smf/Themes/RedLine/languages/Modifications.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 1.1; Modifications
+$txt['uot_users_online_today']='Users Logged In Today';
+$txt['uot_yesterday']='Yesterday at ';
+$txt['uot_total']='Total';
+$txt['uot_visible']='Visible';
+$txt['uot_hidden']='Hidden';
+			
+?>
