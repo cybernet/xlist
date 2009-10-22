74b19c98f23762790e18dcdb31299cf44c63d900
cybernet <cybernet2u@2reply.in>
2009-10-22 14:01:56 +0000

    background mach with frosted style :) / 22/10/2009 / GMT +2 / http://tracker.cyberfun.ro/ & http://xList.ro/

--- tracker/images/piechart2.php
+++ tracker/images/piechart2.php
@@ -21,40 +21,40 @@ $show_text = true; // true = show text, false = don't show text.
 $show_parts = false; // true = show parts, false = don't show parts.
 $label_form = 'square'; // 'square' or 'round' label.
 $width = 125;
-$background_color = 'FFFFFF'; // background-color of the chart...
+$background_color = 'F1F4FA'; // background-color of the chart...
 $text_color = '000000'; // text-color.
-$colors = array('003366', 'CCD6E0', '7F99B2','F7EFC6', 'C6BE8C', 'CC6600','990000','520000','BFBFC1','808080'); // colors of the slices.
+$colors = array('003366', 'CCD6E0', '7F99B2','F7EFC6', 'C6BE8C', 'CC6600', '990000', '520000', 'BFBFC1', '808080'); // colors of the slices.
 $shadow_height = 16; // Height on shadown.
 $shadow_dark = true; // true = darker shadow, false = lighter shadow...
 
 // DON'T CHANGE ANYTHING BELOW THIS LINE...
 
-$height = $width/2;
-$data = explode('*',$data);
+$height = $width / 2;
+$data = explode('*', $data);
 
-if ($label != '') $label = explode('*',$label);
+if ($label != '') $label = explode('*', $label);
 
 for ($i = 0; $i < count($label); $i++) 
 {
-	if ($data[$i]/array_sum($data) < 0.1) $number[$i] = ' '.number_format(($data[$i]/array_sum($data))*100,1,',','.').'%';
-	else $number[$i] = number_format(($data[$i]/array_sum($data))*100,1,',','.').'%';
+	if ($data[$i] / array_sum($data) < 0.1) $number[$i] = ' '.number_format(($data[$i] / array_sum($data)) * 100, 1, ',', '.').'%';
+	else $number[$i] = number_format(($data[$i] / array_sum($data)) * 100, 1, ',', '.').'%';
 	if (strlen($label[$i]) > $text_length) $text_length = strlen($label[$i]);
 }
 
 if (is_array($label))
 {
 $antal_label = count($label);
-$xtra = (5+15*$antal_label)-($height+ceil($shadow_height));
-if ($xtra > 0) $xtra_height = (5+15*$antal_label)-($height+ceil($shadow_height));
+$xtra = (5 + 15 * $antal_label) - ($height + ceil($shadow_height));
+if ($xtra > 0) $xtra_height = (5 + 15 * $antal_label) - ($height + ceil($shadow_height));
 
 $xtra_width = 5;
 if ($show_label) $xtra_width += 20;
 if ($show_percent) $xtra_width += 45;
-if ($show_text) $xtra_width += $text_length*8;
+if ($show_text) $xtra_width += $text_length * 8;
 if ($show_parts) $xtra_width += 35;
 }
 
-$img = ImageCreateTrueColor($width+$xtra_width, $height+ceil($shadow_height)+$xtra_height);
+$img = ImageCreateTrueColor($width + $xtra_width, $height + ceil($shadow_height) + $xtra_height);
 
 ImageFill($img, 0, 0, colorHex($img, $background_color));
 
@@ -72,29 +72,29 @@ for ($i = 0; $i < count($label); $i++)
 {
 	if ($label_form == 'round' && $show_label)
 	{
-		imagefilledellipse($img,$width+11,$label_place+5,10,10,colorHex($img, $colors[$i % count($colors)]));
+		imagefilledellipse($img, $width + 11, $label_place + 5, 10, 10, colorHex($img, $colors[$i % count($colors)]));
 		imageellipse($img,$width+11,$label_place+5,10,10,colorHex($img, $text_color));
 	}
 	else if ($label_form == 'square' && $show_label)
 	{	
-		imagefilledrectangle($img,$width+6,$label_place,$width+16,$label_place+10,colorHex($img, $colors[$i % count($colors)]));
-		imagerectangle($img,$width+6,$label_place,$width+16,$label_place+10,colorHex($img, $text_color));
+		imagefilledrectangle($img, $width + 6, $label_place, $width + 16, $label_place + 10, colorHex($img, $colors[$i % count($colors)]));
+		imagerectangle($img, $width + 6, $label_place, $width + 16, $label_place + 10,colorHex($img, $text_color));
 	}
 
 	if ($show_percent) $label_output = $number[$i].' ';
 	if ($show_text) $label_output = $label_output.$label[$i].' ';
 	if ($show_parts) $label_output = $label_output.$data[$i];
 
-	imagestring($img,'2',$width+20,$label_place,$label_output,colorHex($img, $text_color));
+	imagestring($img, '2', $width + 20, $label_place, $label_output, colorHex($img, $text_color));
 	$label_output = '';
 
 	$label_place = $label_place + 15;
 }
 }
-$centerX = round($width/2);
-$centerY = round($height/2);
-$diameterX = $width-4;
-$diameterY = $height-4;
+$centerX = round($width / 2);
+$centerY = round($height / 2);
+$diameterX = $width - 4;
+$diameterY = $height - 4;
 
 $data_sum = array_sum($data);
 
@@ -103,13 +103,13 @@ $start = 270;
 for ($i = 0; $i < count($data); $i++) 
 {
 	$value += $data[$i];
-	$end = ceil(($value/$data_sum)*360) + 270;
+	$end = ceil(($value / $data_sum) * 360) + 270;
 	$slice[] = array($start, $end, $shadow_color[$value_counter % count($shadow_color)], $fill_color[$value_counter % count($fill_color)]);
 	$start = $end;
 	$value_counter++;
 }
 
-for ($i=$centerY+$shadow_height; $i>$centerY; $i--) 
+for ($i = $centerY + $shadow_height; $i > $centerY; $i--) 
 {
 	for ($j = 0; $j < count($slice); $j++)
 	{
@@ -159,7 +159,7 @@ function colorHexshadow($img, $HexColorString, $mork)
 function OutputImage($img) 
 {
 	header('Content-type: image/jpg');
-	ImageJPEG($img,NULL,100);
+	ImageJPEG($img, NULL, 100);
 }
 
 ?>
