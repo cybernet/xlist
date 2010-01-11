e4db02cc0b6a054a6563ee252a146fe633bbd04d
cybernet <cybernet2u@2reply.in>
2010-01-11 20:38:51 +0000

    // copyright changed // // x
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xlist.ro/
    // Modified By cybernet2u

--- tracker/include/cyberfun_footer.php
+++ tracker/include/cyberfun_footer.php
@@ -1,4 +1,4 @@
 <?php
-$cyberfun_footer="[&nbsp;&nbsp;<u>xBTiT FuLLy MoDDeD By cybernet</u>: <a href=\"http://xList.ro/\" target=\"_blank\">xList Tracker</a>&nbsp;]
+$cyberfun_footer = "[&nbsp;&nbsp;<u>xBTiT FuLLy MoDDeD By cybernet</u>: <a href=\"http://xList.ro/\" target=\"_blank\">xList Tracker</a>&nbsp;]
 [&nbsp;&nbsp;<u>xbtit '.$tracker_version.' By</u>: <a href=\"http://www.btiteam.org/\" target=\"_blank\">Btiteam</a>&nbsp;]";
 ?>
