eee99df662c3471e16332e97829d529f0f3d1d42
cybernet <cybernet2u@2reply.in>
2010-01-23 22:53:20 +0000

    // CyBerFuN_xBTiT_Fully_MoDDeD_v1.1 added //
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

---
+++ releases/CyBerFuN_xBTiT_Fully_MoDDeD_v1.1/smf/Themes/dilbermc/languages/Settings.spanish.php
@@ -0,0 +1,34 @@
+<?php
+// Version: 1.1; Settings
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Tema Dilber Multicolor creado por HarzeM.<br /><br />Un tema con un estilo profesional y una carga r&aacute;pida, con muchas opciones de colores.';
+
+$txt['logo_not_show'] = 'No mostrar el logo de arriba';
+
+$txt['page_div_width'] = 'Ancho de p&aacute;gina';
+$txt['page_div_width_desc'] = 'Puede ser en <b>pixeles </b>, como <b>800px</b>, o puede ser un <b>porcentaje</b>, como <b>75%</b>. Debe terminar con <b>px o %</b>';
+$txt['show_name_near_logo'] = 'Mostrar el nombre del foro cerca del logo';
+
+$txt['top_right_html'] = 'C&oacute;digo HTML para ser mostrado en la parte derecha de arriba';
+$txt['top_right_html_desc'] = 'Usted puede usar esto para agregar enlaces a otras webs, o agregar banners etc.';
+
+$txt['custom_global_html'] = 'C&oacute;digo HTML a ser mostrado debajo de la barra de informaci&oacute;n de usuario';
+$txt['custom_global_html_desc'] = 'C&oacute;digo HTML a ser mostrado encima del contenido del foro, debajo de la barra de informaci&oacute;n del usuario. Usted puede usar esto para agregar un banner o crear mensajes globales etc.';
+
+$txt['child_boards_rows'] = 'Mostrar los subforos en una misma celda, sin tener dos';
+$txt['child_boards_rows_desc'] = 'Si est&aacute; seleccionado, la lista de subforos ser&aacute; una envez de dos celdad..';
+
+$txt['boardname_posts_lastposts_nodisplay'] = 'No mostrar barras de "nombre del foro", "mensajes", "&uacute;ltimos mensajes" en el &iacute;ndice del foro';
+$txt['boardname_posts_lastposts_nodisplay_desc'] = 'Si seleccionas, "nombre del foro", "mensajes", "&uacute;ltimos mensajes" en el foro estos ser&aacute;n removidos.';
+
+$txt['custom_copyright'] = 'Texto de derechos al ser mostrados abajo de los derechos de SMF';
+$txt['custom_copyright_desc'] = 'Usted puede usar c&oacute;digos HTML aqu&iacute;';
+
+$txt['allow_color_change'] = '¿Permitir cambiar el color en la p&aacute;gina principal?';
+$txt['allow_color_change_no'] = 'No permitir';
+$txt['allow_color_change_box'] = 'Mostrarlo en una caja de selecci&oacute;n';
+$txt['allow_color_change_bars'] = 'Mostrar como barras';
+
+$txt['theme_main_color'] = 'Color del Tema';
+?>
---
+++ releases/CyBerFuN_xBTiT_Fully_MoDDeD_v1.1/style/NB-005/style_copyright.php
@@ -0,0 +1,3 @@
+<?php
+$design_copyright = "[&nbsp;&nbsp;<u>xList Tracker By</u>: <a href=\"#\" target=\"_blank\">cybernet</a>&nbsp;]";
+?>
