c9cc32af9ac0a3bbcf56b4677898937de615ed98
cybernet <cybernet2u@2reply.in>
2010-02-05 12:55:17 +0000

    // CyBerFuN xBTiT Fully MoDDeD v1.1.2 released from the chains :))
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

---
+++ releases/CyBerFuN_xBTiT_Fully_MoDDeD_v1.1.2/smf/Themes/dilbermc/languages/Settings.norwegian.php
@@ -0,0 +1,34 @@
+<?php
+// Version: 1.1; Settings
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Dilber Multicolor theme av HarzeM.<br /><br />Et profesjonelt design med flere fargemuligheter.';
+
+$txt['logo_not_show'] = 'Ikke vis logo p&#229; toppen';
+
+$txt['page_div_width'] = 'Sidens bredde';
+$txt['page_div_width_desc'] = 'Kan v&aelig;re i <b>piksler</b>, som <b>800px</b>, eller i <b>prosent</b>, som <b>75%</b>. Skal ende p&aring; <b>enten px eller %</b>';
+$txt['show_name_near_logo'] = 'Vis forumnavn n&aelig;r logo';
+
+$txt['top_right_html'] = 'HTML kode som vises p&aring; h&oslash;yre topp';
+$txt['top_right_html_desc'] = 'Du kan bruke det til &aring; vise linker, banner o.l.';
+
+$txt['custom_global_html'] = 'HTML kode som vises <b>under</b> info-senteret';
+$txt['custom_global_html_desc'] = 'HTML koden vil vises over forumet, under brukerens infobar. Her kan du legge til bannere, viktige saker o.l.';
+
+$txt['child_boards_rows'] = 'Vis underforum p&aring; en linje, istedenfor to';
+$txt['child_boards_rows_desc'] = 'Hvis valgt, vil underforum listningen vise p&aring; en linje.';
+
+$txt['boardname_posts_lastposts_nodisplay'] = 'Ikke vis "forumnavn", "poster", "sistepost" titler i listen';
+$txt['boardname_posts_lastposts_nodisplay_desc'] = 'Hvis valgt, vil "forumnavn", "poster", "sistepost" titler under forumlistningen forsvinne.';
+
+$txt['custom_copyright'] = 'Tekst som skal vises <b>under</b> SMF Copyright';
+$txt['custom_copyright_desc'] = 'Du kan bruke HTML kode her.<br/><br/><hr/><hr/><br/>';
+
+$txt['allow_color_change'] = 'Tillate fargevalg p&aring; framside?';
+$txt['allow_color_change_no'] = 'Ikke tillate';
+$txt['allow_color_change_box'] = 'Vis som rullegardinsmeny';
+$txt['allow_color_change_bars'] = 'Vis som fargebokser';
+
+$txt['theme_main_color'] = 'Designets farge';
+?>
