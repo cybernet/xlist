a2fdb97ac7d997cdac37380e99aa3c9d9377709a
cybernet <cybernet2u@2reply.in>
2010-02-08 22:19:07 +0000

    // updates
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

--- releases/xList_ro/include/CyBerFuN_xBTiT_version.php
+++ releases/xList_ro/include/CyBerFuN_xBTiT_version.php
@@ -1,3 +1,24 @@
 <?php
-$CyBerFuN_xBTiT_version = 'v1.1.2 ( xList.ro rev 216 )';
+$CyBerFuN_xBTiT_version = "v1.1.2 ( xList.ro rev 216 )<br />
+<script type=\"text/javascript\">
+var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
+</script>
+<script type=\"text/javascript\">
+try {
+var pageTracker = _gat._getTracker(\"UA-10155921-2\");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- Piwik -->
+<script type=\"text/javascript\">
+var pkBaseURL = ((\"https:\" == document.location.protocol) ? \"https://stats.xlist.ro/\" : \"http://stats.xlist.ro/\");
+document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\"));
+</script><script type=\"text/javascript\">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src=\"http://stats.xlist.ro/piwik.php?idsite=1\" style=\"border:0\" alt=\"\"/></p></noscript>
+<!-- End Piwik Tag -->";
 ?>
--- releases/xList_ro/include/cyberfun_footer.php
+++ releases/xList_ro/include/cyberfun_footer.php
@@ -1,3 +1,25 @@
 <?php
-$design_copyright = '[&nbsp;&nbsp;<u>CyBerFuN xBTiT ' . $CyBerFuN_xBTiT_version . ' By cybernet</u>: <a href="http://xList.ro/" target="_blank">xList Tracker</a>&nbsp;]<br /> [&nbsp;&nbsp;<u>xbtit '.$tracker_version.' By <a href="http://www.btiteam.org/" target="_blank">BTiTeam.org</a></u>&nbsp;]<br />';
+$design_copyright = '[&nbsp;&nbsp;<u>CyBerFuN xBTiT ' . $CyBerFuN_xBTiT_version . ' By cybernet</u>: <a href="http://xList.ro/" target="_blank">xList Tracker</a>&nbsp;]<br /> [&nbsp;&nbsp;<u>xbtit '.$tracker_version = "By <a href=\"http://www.btiteam.org/\" target=\"_blank\">BTiTeam.org</a></u>&nbsp;]<br />
+v1.1.2 ( xList.ro rev 216 )<br />
+<script type=\"text/javascript\">
+var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
+</script>
+<script type=\"text/javascript\">
+try {
+var pageTracker = _gat._getTracker(\"UA-10155921-2\");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- Piwik -->
+<script type=\"text/javascript\">
+var pkBaseURL = ((\"https:\" == document.location.protocol) ? \"https://stats.xlist.ro/\" : \"http://stats.xlist.ro/\");
+document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\"));
+</script><script type=\"text/javascript\">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src=\"http://stats.xlist.ro/piwik.php?idsite=1\" style=\"border:0\" alt=\"\"/></p></noscript>
+<!-- End Piwik Tag -->";
 ?>
