8aeede94412745b78caf469e9611c63fb76cdede
cybernet <cybernet2u@2reply.in>
2010-02-25 21:55:54 +0000

    // ubuntu 9.10 x64 fresh install
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

---
+++ cybernet2u/stuff/shit.php
@@ -0,0 +1,14 @@
+<?php
+$owner = cybernet;
+echo "ubuntu 9.10 x64 fresh install by $owner";
+
+// Have FuN
+
+// CyBerFuN.ro & xList.ro
+
+// CyBerFuN .::. xList //
+// http://tracker.cyberfun.ro/
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// Modified By cybernet2u"
+?>
