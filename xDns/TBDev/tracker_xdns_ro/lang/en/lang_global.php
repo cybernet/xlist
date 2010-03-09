ccad27864f69e187570b817b625807cd69007909
cybernet <cybernet2u@2reply.in>
2010-03-09 05:56:42 +0000

    // Tue 09 Mar 2010 07:52:56 AM EET - tracker_xdns_ro released from chains lol
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

---
+++ xDns/TBDev/tracker_xdns_ro/lang/en/lang_global.php
@@ -0,0 +1,52 @@
+<?php
+
+$lang = array (
+
+#header
+'gl_donate'	      => "donate",
+
+#menu
+
+'gl_home' 			=> "RETURN TO<br/>MAINPAGE....",
+'gl_home2' 			=> "RETURN TO<br/>MAINPAGE....",
+'gl_browse' 			=> "VIEW ALL<br/>TORRENT",
+'gl_search' 			=> "LOOKING FOR<br/>SOMETHING?",
+'gl_upload' 			=> "FEED THE<br/>SITE HERE..",
+'gl_chat' 			=> "HAVE A LITTLE<br/>CHAT HERE",
+'gl_forums' 			=> "SEEK AND YOU<br/>WILL FIND HERE",
+'gl_top_10' 			=> "SEE THE<br/>SITE STAT...!",
+'gl_rules' => "PLEASE READ<br/>THIS NOW",
+'gl_faq' => "READ BEFORE<br/>ASKING ANY",
+'gl_links' => "PLEASE VISIT<br/>OUR FRIENDS",
+'gl_staff' => "GET YOUR<br/>SUPPORT",
+
+#Mod admin link
+'gl_admin' 			  => "Admin",
+
+#Logged in links
+'gl_profile'			=>	"profile",
+'gl_logout' =>	"logout",
+
+#a guest user links
+'gl_login'				=>	"Log In",
+'gl_signup'				=>	"Sign Up",
+'gl_recover'			=>	"Recover Account",
+
+#big red message thang
+'gl_msg_alert'		=>	"You have %s new message",
+
+#statusbar
+'gl_msg_singular'		  =>	"New Message",
+'gl_msg_plural'			  =>	"New Messages",
+
+'gl_msg_welcome'		  =>	"Welcome back",
+'gl_ratio'				    =>	"Ratio",
+'gl_uploaded'			    =>	"Uploaded",
+'gl_downloaded'				=>	"Downloaded",
+'gl_act_torrents'			=>	"Active Torrents",
+'gl_seed_torrents'		=>	"Seeding",
+'gl_leech_torrents'		=>	"Leeching",
+
+'gl_time_date' => ""
+);
+?>
