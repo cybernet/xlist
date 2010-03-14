f335d05bbdb731efcb62fa9e3ea4e9b54554551f
cybernet <cybernet2u@2reply.in>
2010-03-14 20:43:58 +0000

    // Sun 14 Mar 2010 10:42:54 PM EET - 09 Kidvision UniquePixels 1.0 (Final) installed // support for this mod - http://bit.ly/czfomq
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

--- xDns/TBDev/tracker_xdns_ro/templates/1/template.php
+++ xDns/TBDev/tracker_xdns_ro/templates/1/template.php
@@ -1,10 +1,4 @@
 <?php
-
-/*
-**Template Mod By AronTh for TBDEV.NET source code 2009, theme made by AronTh
-**Special Thanks to CoLdFuSiOn for providing the source code and KiD for the motivation to me(AronTh) to make themes for TbDev.net
-*/
-
 function stdhead($title = "", $msgalert = true) {
     global $CURUSER, $TBDEV, $lang;
 
@@ -25,127 +19,70 @@ function stdhead($title = "", $msgalert = true) {
       $unread = $arr[0];
     }
 
-    $htmlout = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
-		\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-		
-		<html xmlns='http://www.w3.org/1999/xhtml'>
-		<head>
-
-			<meta name='generator' content='TBDev.net' />
-			<meta http-equiv='Content-Language' content='en-us' />
-			<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-			<meta name='MSSmartTagsPreventParsing' content='TRUE' />
-			
-			<title>{$title}</title>
-			<link rel='stylesheet' href='templates/1/1.css' type='text/css' />
-		</head>
-    
+       $htmlout = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+    \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+    <!-- *************************************************************************************** -->
+    <!--                    KIDVISION UNIQUEPIXELS 1.0 | www.kidvision.me                        -->
+    <!-- *************************************************************************************** -->
+    <html xmlns='http://www.w3.org/1999/xhtml'>
+    <head>
+    <meta name='generator' content='TBDev.net' />
+    <meta http-equiv='Content-Language' content='en-us' />
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <meta name='MSSmartTagsPreventParsing' content='TRUE' />
+    <title>{$title}</title>
+    <link rel='stylesheet' href='templates/1/1.css' type='text/css' />
+    <script type='text/javascript' src='scripts/java_klappe.js'></script>
+    </head>
     <body>
-
-      <table width='100%' cellspacing='0' cellpadding='0' style='background: transparent'>
-      <tr>
-
-      <td class='clear'>
-      <div id='logostrip'>
-      <img src='{$TBDEV['pic_base_url']}logo.jpg' alt='' />
-
-      <a href='donate.php'><img src='{$TBDEV['pic_base_url']}x-click-but04.gif' border='0' alt='{$lang['gl_donate']}' title='{$lang['gl_donate']}' style='margin-top: 5px' /></a>
-      </div>
-      </td>
-
-      </tr></table>
-
-      <table class='mainouter' width='100%' border='1' cellspacing='0' cellpadding='10'>
-<!-- STATUSBAR -->";
-
-    $htmlout .= StatusBar();
-
-    $htmlout .= "<!-- MENU -->
-      <tr><td class='outer'>
-      <div id='submenu'>";
-
+    <table width='950' cellspacing='0' cellpadding='0' style='background: transparent' align='center'>
+    <tr>
+    <td class='clear'>
+    <div id='logostrip'>";
     if ($CURUSER) 
     { 
-      $htmlout .= "<div class='tb-top-left-link'>
-      <a href='index.php'>{$lang['gl_home']}</a>
-      <a href='browse.php'>{$lang['gl_browse']}</a>
-      <a href='search.php'>{$lang['gl_search']}</a>
-      <a href='upload.php'>{$lang['gl_upload']}</a>
-      <a href='chat.php'>{$lang['gl_chat']}</a>
-      <a href='forums.php'>{$lang['gl_forums']}</a>
-      <!--<a href='misc/dox.php'>DOX</a>-->
-      <a href='topten.php'>{$lang['gl_top_10']}</a>
-      <a href='rules.php'>{$lang['gl_rules']}</a>
-      <a href='faq.php'>{$lang['gl_faq']}</a>
-      <a href='links.php'>{$lang['gl_links']}</a>
-      <a href='staff.php'>{$lang['gl_staff']}</a>
-      </div>
-      <div class='tb-top-right-link'>";
-
-      if( $CURUSER['class'] >= UC_MODERATOR )
-      {
-        $htmlout .= "<a href='admin.php'>{$lang['gl_admin']}</a>";
-      }
-
-    $htmlout .= "<a href='my.php'>{$lang['gl_profile']}</a>
-      <a href='logout.php'>{$lang['gl_logout']}</a>
-      </div>";
-    } 
-    else
-    {
-      $htmlout .= "<div class='tb-top-left-link'>
-      <a href='login.php'>{$lang['gl_login']}</a>
-      <a href='signup.php'>{$lang['gl_signup']}</a>
-      <a href='recover.php'>{$lang['gl_recover']}</a>
-      </div>";
+    $htmlout .= "
+    <ul id='iconbar'>
+    <li><a href='{$TBDEV['baseurl']}/index.php'>{$lang['gl_home']}</a></li>
+    <li><a href='{$TBDEV['baseurl']}/browse.php'>{$lang['gl_browse']}</a></li>
+    <li><a href='{$TBDEV['baseurl']}/search.php'>{$lang['gl_search']}</a></li>
+    <li><a href='{$TBDEV['baseurl']}/upload.php'>{$lang['gl_upload']}</a></li>
+    <li><a href='{$TBDEV['baseurl']}/forums.php'>{$lang['gl_forums']}</a></li>
+    <li><a href='{$TBDEV['baseurl']}/rules.php'>{$lang['gl_rules']}</a></li>
+    <li><a href='{$TBDEV['baseurl']}/credits.php'>CREDITS</a></li>
+    <li><a href='{$TBDEV['baseurl']}/my.php'>{$lang['gl_profile']}</a></li>
+    ";
+        if (get_user_class() >= UC_POWER_USER)
+        {
+    $htmlout .= "
+    <li><a href='{$TBDEV['baseurl']}/staff.php'>{$lang['gl_staff']}</a></li>";
     }
-
-    $htmlout .= "</div>
-    </td>
-    </tr>
+    if( $CURUSER['class'] >= UC_MODERATOR )
+    $htmlout .= "
+    <li><a href='{$TBDEV['baseurl']}/admin.php'>{$lang['gl_admin']}</a></li>";
+    $htmlout .= "</ul><br />"; 
+    }     
+    $htmlout .= StatusBar(); 
+    $htmlout .="</div></td></tr></table><br /><br />
+    <table class='mainouter' width='100%' border='0' cellspacing='0' cellpadding='10'>
     <tr><td align='center' class='outer' style='padding-top: 20px; padding-bottom: 20px'>";
-
-
-    if ($TBDEV['msg_alert'] && isset($unread) && !empty($unread))
-    {
-      $htmlout .= "<p><table border='0' cellspacing='0' cellpadding='10' bgcolor='red'>
-                  <tr><td style='padding: 10px; background: red'>\n
-                  <b><a href='messages.php'><font color='white'>".sprintf($lang['gl_msg_alert'], $unread) . ($unread > 1 ? "s" : "") . "!</font></a></b>
-                  </td></tr></table></p>\n";
-    }
-
-    return $htmlout;
     
-} // stdhead
+   if ($TBDEV['msg_alert'] && isset($unread) && !empty($unread))
+   {
+   $htmlout .= "<table border='0' cellspacing='0' cellpadding='10' bgcolor='red'>
+   <tr><td style='padding: 10px; background: #890537'>\n
+   <b><a href='{$TBDEV['baseurl']}/messages.php'><font color='white'>".sprintf($lang['gl_msg_alert'], $unread) . ($unread > 1 ? "s" : "") . "!</font></a></b>
+   </td></tr></table><br />\n";
+   }
+   return $htmlout;
+   } // 
 
 function stdfoot() {
   global $TBDEV;
-  
-    return "<p align='center'>
-    <a rel='license' href='http://creativecommons.org/licenses/by-nc-nd/2.0/uk/'><img alt='Creative Commons License' style='border-width:0' src='http://i.creativecommons.org/l/by-nc-nd/2.0/uk/88x31.png' /></a><br />
-    <a href='http://www.tbdev.net'><img src='{$TBDEV['pic_base_url']}tbdev_btn_red.png' border='0' alt='Powered By TBDev &copy;2009' title='Powered By TBDev &copy;2009' /></a></p>
-<script type=\"text/javascript\">
-var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
-document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
-</script>
-<script type=\"text/javascript\">
-try {
-var pageTracker = _gat._getTracker(\"UA-13256113-1\");
-pageTracker._trackPageview();
-} catch(err) {}</script>\n
-<!-- Piwik -->
-<script type=\"text/javascript\">
-var pkBaseURL = ((\"https:\" == document.location.protocol) ? \"https://stats.xdns.ro/\" : \"http://stats.xdns.ro/\");
-document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\"));
-</script><script type=\"text/javascript\">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", 4);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src=\"http://stats.xdns.ro/piwik.php?idsite=4\" style=\"border:0\" alt=\"\"/></p></noscript>
-<!-- End Piwik Tag -->\n
-    </td></tr></table>\n
+  return 
+  base64_decode("PHAgYWxpZ249J2NlbnRlcic+PGEgaHJlZj0naHR0cDovL3ZhbGlkYXRvci53My5vcmcnPjxpbWcgc3JjPSdwaWMvbG9naW4veGh0bWxfdmFsaWQucG5nJyBhbHQ9J1hodG1sIHZhbGlkJy8+PC9hPiZuYnNwOyZuYnNwOzxhIGhyZWY9J2h0dHA6Ly9qaWdzYXcudzMub3JnL2Nzcy12YWxpZGF0b3IvY2hlY2svJz48aW1nIHNyYz0ncGljL2xvZ2luL2Nzc192YWxpZC5wbmcnIGFsdD0nWGh0bWwgdmFsaWQnIC8+PC9hPiZuYnNwOyZuYnNwOzxhIGhyZWY9J2h0dHA6Ly90YmRldi5uZXQnPjxpbWcgc3JjPSdwaWMvbG9naW4vdGJkZXZfcG93ZXIucG5nJyBhbHQ9J1RCREVWJy8+PC9hPiZuYnNwOyZuYnNwOzxhIGhyZWY9J2h0dHA6Ly9raWR2aXNpb24ubWUnPjxpbWcgc3JjPSdwaWMvbG9naW4va2lkdmlzaW9uX2Rlc2lnbi5wbmcnIGFsdD0nRGVzaWduJy8+PC9hPg==");
+  $htmlout .="
+  </td></tr></table>\n
     </body></html>\n";
 }
 
@@ -182,13 +119,13 @@ function StatusBar() {
 	$IsDonor = '';
 	if ($CURUSER['donor'] == "yes")
 	
-	$IsDonor = "<img src='pic/star.gif' alt='donor' title='donor' />";
+	$IsDonor = "<img src='pic/theme/star.png' alt='donor' title='donor' />";
 
 
 	$warn = '';
 	if ($CURUSER['warned'] == "yes")
 	
-	$warn = "<img src='pic/warned.gif' alt='warned' title='warned' />";
+	$warn = "<img src='pic/theme/warn.png' alt='warned' title='warned' />";
 	
 	$res1 = @mysql_query("SELECT COUNT(*) FROM messages WHERE receiver=" . $CURUSER["id"] . " AND unread='yes'") or sqlerr(__LINE__,__FILE__);
 	
@@ -218,26 +155,12 @@ function StatusBar() {
 ////////////// REP SYSTEM END //////////
 
 	$StatusBar = '';
-		$StatusBar = "<tr>".
-
-		"<td colspan='2' style='padding: 2px;'>".
-
-		"<div id='statusbar'>".
-		"<div style='float:left;color:black;'>{$lang['gl_msg_welcome']}, <a href='userdetails.php?id={$CURUSER['id']}'>{$CURUSER['username']}</a>".
-		  
-		"$IsDonor$warn&nbsp; [<a href='logout.php'>{$lang['gl_logout']}</a>]&nbsp;$member_reputation
-		<br />{$lang['gl_ratio']}:$ratio".
-		"&nbsp;&nbsp;{$lang['gl_uploaded']}:$upped".
-		"&nbsp;&nbsp;{$lang['gl_downloaded']}:$downed".
-		
-		"&nbsp;&nbsp;{$lang['gl_act_torrents']}:&nbsp;<img alt='{$lang['gl_seed_torrents']}' title='{$lang['gl_seed_torrents']}' src='pic/arrowup.gif' />&nbsp;{$seedleech['yes']}".
-		
-		"&nbsp;&nbsp;<img alt='{$lang['gl_leech_torrents']}' title='{$lang['gl_leech_torrents']}' src='pic/arrowdown.gif' />&nbsp;{$seedleech['no']}</div>".
-    
-		"<div><p style='text-align:right;'>".date(DATE_RFC822)."<br />".
-
-    "<a href='messages.php'>$inbox</a></p></div>".
-    "</div></td></tr>";
+		$StatusBar = 
+		"<div id='statusbar'>
+        {$lang['gl_msg_welcome']}, <a href='userdetails.php?id={$CURUSER['id']}'>{$CURUSER['username']}</a>
+		$IsDonor$warn&nbsp;|&nbsp;[<a href='logout.php'>logout</a>]&nbsp;&nbsp;|&nbsp;&nbsp;Invites:&nbsp;<a href='{$TBDEV['baseurl']}/invite.php'>{$CURUSER['invites']}</a>
+		&nbsp;&nbsp;|&nbsp;&nbsp;{$lang['gl_uploaded']}: {$upped}<!--&nbsp;&nbsp;|&nbsp;&nbsp;{$lang['gl_ratio']}:{$ratio}-->&nbsp;&nbsp;|&nbsp;&nbsp;<a href='messages.php'>$inbox</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://twitter.com/kidvisionme' target='_blank'><img src='templates/1/pic/twitter.png' alt='Twitter' /></a>&nbsp;&nbsp;<a href='http://www.facebook.com/kidvisionme' target='_blank'><img src='templates/1/pic/facebook.png' alt='Facebook' /></a>
+  </div>";
 	
 	return $StatusBar;
 
