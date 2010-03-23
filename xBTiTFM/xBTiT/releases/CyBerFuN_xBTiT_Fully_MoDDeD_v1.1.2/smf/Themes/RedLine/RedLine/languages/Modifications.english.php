21087ca398107503110122f5c79bc55d5ce4deec
cybernet <cybernet2u@2reply.in>
2010-03-23 19:28:07 +0000

    // Tue 23 Mar 2010 09:27:01 PM EET - removed
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

--- xBTiTFM/xBTiT/releases/CyBerFuN_xBTiT_Fully_MoDDeD_v1.1.2/smf/Themes/RedLine/RedLine/languages/Modifications.english.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Version: 1.1; Modifications
-$txt['uot_users_online_today']='Users Logged In Today';
-$txt['uot_yesterday']='Yesterday at ';
-$txt['uot_total']='Total';
-$txt['uot_visible']='Visible';
-$txt['uot_hidden']='Hidden';
-			
-?>
