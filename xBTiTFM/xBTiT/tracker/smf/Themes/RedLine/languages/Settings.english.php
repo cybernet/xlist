166961c8163a2da704284ba05db7f053d51920a8
cybernet <cybernet2u@2reply.in>
2010-03-23 19:38:25 +0000

    // Tue 23 Mar 2010 09:38:03 PM EET - removed
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

--- xBTiTFM/xBTiT/tracker/smf/Themes/RedLine/languages/Settings.english.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Version: 1.1; Settings
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'The XBTIT Default Theme.<br /><br />Thanks go to TreetopClimber.';
-
-?>
